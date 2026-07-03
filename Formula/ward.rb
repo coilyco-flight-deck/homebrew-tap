04d04410132b95913c9007839b81db338eb8e800
forgejo-tap-writer <coilyco-ops@coilysiren.me>
2026-07-03 10:14:15 +0000

    chore(formula): bump ward to v0.356.0 [skip ci]

--- Formula/ward.rb
+++ Formula/ward.rb
@@ -1,8 +1,8 @@
 class Ward < Formula
   desc "A contributor-facing cli-guard consumer"
   homepage "https://forgejo.coilysiren.me/coilyco-flight-deck/ward"
-  url "https://forgejo.coilysiren.me/coilyco-flight-deck/ward/archive/v0.355.0.tar.gz"
-  sha256 "424a284840852f9629b6dc5d394faa6f3e81b1a7a119f4c822a7d158d8ea7f46"
+  url "https://forgejo.coilysiren.me/coilyco-flight-deck/ward/archive/v0.356.0.tar.gz"
+  sha256 "90a2045c967431a8ceaf37a708cbea191e24de68b8d75be226622f10178ed375"
   license "MIT"
   head "https://forgejo.coilysiren.me/coilyco-flight-deck/ward.git", branch: "main"
 
