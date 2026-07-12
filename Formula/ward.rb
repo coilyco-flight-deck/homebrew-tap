c8399eae4a110027b5ece77e381919564b2084b6
forgejo-tap-writer <coilyco-ops@coilysiren.me>
2026-07-12 06:16:42 +0000

    chore(formula): bump ward to v0.630.0 [skip ci]

--- Formula/ward.rb
+++ Formula/ward.rb
@@ -1,26 +1,26 @@
 class Ward < Formula
   desc "A contributor-facing cli-guard consumer"
   homepage "https://forgejo.coilysiren.me/coilyco-flight-deck/ward"
-  version "0.629.0"
+  version "0.630.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://forgejo.coilysiren.me/coilyco-flight-deck/ward/releases/download/v0.629.0/ward-darwin-arm64"
-      sha256 "85d0dba3fbea9867ebdd195a08fe712fb3728dbb648b0b01507a35b66e988486"
+      url "https://forgejo.coilysiren.me/coilyco-flight-deck/ward/releases/download/v0.630.0/ward-darwin-arm64"
+      sha256 "5c98b332782dde48a3c9ebb88f84ec985184c7cc739ab5a0dab294dbf3d6f160"
     else
-      url "https://forgejo.coilysiren.me/coilyco-flight-deck/ward/releases/download/v0.629.0/ward-darwin-amd64"
-      sha256 "2bd305b11b6e065d714345f58bdaa3e58437a068a12781597b7c12a5bd5aa0dd"
+      url "https://forgejo.coilysiren.me/coilyco-flight-deck/ward/releases/download/v0.630.0/ward-darwin-amd64"
+      sha256 "77143868ca50e5b0b5f4ec758c5181460fc8e4292e5b4f3e30f49189fea5b4d4"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://forgejo.coilysiren.me/coilyco-flight-deck/ward/releases/download/v0.629.0/ward-linux-arm64"
-      sha256 "b86d07442d9f49fee2c2fdc3bcc89f00406d99c4729047ae29a8b295631ef58e"
+      url "https://forgejo.coilysiren.me/coilyco-flight-deck/ward/releases/download/v0.630.0/ward-linux-arm64"
+      sha256 "4d0e4a61c6335d0140f8fe6d5433b6d823624cea7300af84dafbcd4cd03d0129"
     else
-      url "https://forgejo.coilysiren.me/coilyco-flight-deck/ward/releases/download/v0.629.0/ward-linux-amd64"
-      sha256 "dd32dfb97d462dfb202b6dea97a6502626fe780f3808c28cd77d48a15d1b3f18"
+      url "https://forgejo.coilysiren.me/coilyco-flight-deck/ward/releases/download/v0.630.0/ward-linux-amd64"
+      sha256 "44f7489af7f43bba37605e9655d3637f2fced00e992f2651e558438a0280c4d4"
     end
   end
 
