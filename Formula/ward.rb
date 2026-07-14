205b885e5d9e327d8cb0e24650dbccc43ef0de6a
forgejo-tap-writer <coilyco-ops@coilysiren.me>
2026-07-14 11:01:15 +0000

    chore(formula): bump ward to v0.680.0 [skip ci]

--- Formula/ward.rb
+++ Formula/ward.rb
@@ -1,26 +1,26 @@
 class Ward < Formula
   desc "A contributor-facing cli-guard consumer"
   homepage "https://forgejo.coilysiren.me/coilyco-flight-deck/ward"
-  version "0.679.0"
+  version "0.680.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://forgejo.coilysiren.me/coilyco-flight-deck/ward/releases/download/v0.679.0/ward-darwin-arm64"
-      sha256 "50ba1e4923a122f539258a5377a164d11ecdd265c80c2f70bae4b5d50041ebe7"
+      url "https://forgejo.coilysiren.me/coilyco-flight-deck/ward/releases/download/v0.680.0/ward-darwin-arm64"
+      sha256 "f6e5d2f74ea2abf9d558aafa6398bc4f9df45f1a5d0b7a54b052273928ef0657"
     else
-      url "https://forgejo.coilysiren.me/coilyco-flight-deck/ward/releases/download/v0.679.0/ward-darwin-amd64"
-      sha256 "f7d02d65d401a8bcd8b6b4b1fe59c800b0faef5d83ba1e66403554af15d45c82"
+      url "https://forgejo.coilysiren.me/coilyco-flight-deck/ward/releases/download/v0.680.0/ward-darwin-amd64"
+      sha256 "bb71d57ccea13d4ca6f6e0fa8dc93bb4f4741ac9d22e51be7ecb5a447de97e46"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://forgejo.coilysiren.me/coilyco-flight-deck/ward/releases/download/v0.679.0/ward-linux-arm64"
-      sha256 "3894d075936a6034b121b3defecf434deea717773ea2d2779be32fba2b805b31"
+      url "https://forgejo.coilysiren.me/coilyco-flight-deck/ward/releases/download/v0.680.0/ward-linux-arm64"
+      sha256 "c17f38ebbd94bdda8dbb0f893a7b08bff36efbed3d42fa021059249636055ce3"
     else
-      url "https://forgejo.coilysiren.me/coilyco-flight-deck/ward/releases/download/v0.679.0/ward-linux-amd64"
-      sha256 "bbed3b6ba2321727096607b91b67f2e943efb1270ce871c7bbd0c7028bddbe7b"
+      url "https://forgejo.coilysiren.me/coilyco-flight-deck/ward/releases/download/v0.680.0/ward-linux-amd64"
+      sha256 "66b2d527bf6b2b5d9b5fd59b0f987dfb1ab0cf5f27b7d1013c3a4b3ee3e248df"
     end
   end
 
